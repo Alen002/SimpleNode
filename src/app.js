0f1ed167add0066346eeb6ad17ce7f5468f7a288
Alen <alechkr@gmail.com>
2020-04-09 17:51:27 +0200

    added css and html files

--- src/app.js
+++ src/app.js
@@ -4,22 +4,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-console.log(__dirname); //path to the directory where the current script lives in, in this case /src
-console.log(path.join(__dirname, '../public')); //path and filename where the current script lives in, in this case /src/app.js
+// This starts up the server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
-
-const publicAboutPage = path.join(__dirname, '../public/about.html') 
-app.use(express.static(publicAboutPage)) 
-
-const publicHelpPage = path.join(__dirname, '../public/help.html') 
-app.use(express.static(publicHelpPage)) 
-
-
+/* console.log(__dirname); //path to the directory where the current script lives in, in this case /src
+console.log(path.join(__dirname, '../public')); //path and filename where the current script lives in, in this case /src/app.js */
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))    //use, a way to customize the server
 
-
+app.get('/weather', (req, res) => {
+    //http://localhost:3000/help  
+        res.send({
+            forecast: 23,
+            location: 'Orlando'
+         });
+    });
 
 
 
@@ -49,19 +51,12 @@ app.get('/about', (req, res) => {
     res.send('<h1>TITLE</h1>');
 }); */
 
-app.get('/weather', (req, res) => {
-//http://localhost:3000/help  
-    res.send({
-        forecast: 23,
-        location: 'Orlando'
-     });
-});
+/* const publicAboutPage = path.join(__dirname, '../public/about.html') 
+app.use(express.static(publicAboutPage)) 
 
+const publicHelpPage = path.join(__dirname, '../public/help.html') 
+app.use(express.static(publicHelpPage))  */
 
-// This starts up the server
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
 
 
 
